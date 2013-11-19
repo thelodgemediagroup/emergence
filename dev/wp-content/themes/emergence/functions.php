86766482190d5a0fd975ea65921a544481134e34
Jesse Wozniak <wozniak.jesse@gmail.com>
2013-11-19 16:47:58 -0500

    Cubepoints, digg widget fix, buddypress

--- dev/wp-content/themes/emergence/functions.php
+++
@@ -1,8 +0,0 @@
-<?php 
-//this is the folder that houses the function files to include
-define('child_functions', STYLESHEETPATH . '/functions');
-
-//Get the functions to load all the various templates
-require_once(child_functions . '/load-templates.php');
-
-?>
